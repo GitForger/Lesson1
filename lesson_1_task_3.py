26e9ce15d942ceefd03f82828b51aa8fcfefa64c
GitForger <trueforgefather@gmail.com>
2025-09-09 23:22:43 +0700

    Update lesson_1_task_3.py

--- lesson_1_task_3.py
+++ lesson_1_task_3.py
@@ -1,3 +1,4 @@
-first_name = input("введите имя"),
-last_name = input("введите фамилию")
-print("вас зовут", first_name, last_name)
+first_name = input("введите имя: ")
+last_name = input("введите фамилию: ")
+
+print("вас зовут:", first_name, last_name)
